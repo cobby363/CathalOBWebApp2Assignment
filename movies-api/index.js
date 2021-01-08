ad709ce4a0ec1da43207ab14dee62415aae4e2c0
Cathal <47002668+cobby363@users.noreply.github.com>
2021-01-08 20:57:55 +0000

    Tring to start up the assignment in a new place

---
+++ movies-api/index.js
@@ -0,0 +1,59 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
+import './db';
+import {loadUsers, loadMovies} from './seedData';
+import usersRouter from './api/users';
+import session from 'express-session';
+//import authenticate from './authenticate';
+import passport from './authenticate';
+
+dotenv.config();
+
+const errHandler = (err, req, res,) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
+
+if (process.env.SEED_DB) {
+  loadUsers();
+  loadMovies();
+}
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(express.static('public'));
+//app.use('/api/movies', moviesRouter);
+
+app.use('/api/users', usersRouter);
+app.use(passport.initialize());
+
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+
+app.use(errHandler);
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
+
+//update /api/Movie route
+//app.use('/api/movies', authenticate, moviesRouter);
+
+//app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use('/api/movies',  moviesRouter);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Web App Dev 2: Web API Assignment
+
+WAD2 module, BAc.Entertainment Syatems
